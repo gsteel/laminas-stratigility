e7c8a54797567fcca5c382006b602fe53c954e0a
George Steel <george@net-glue.co.uk>
2024-07-17 12:26:56 +0100

    Add a description for `IterableMiddlewarePipeInterface`
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/IterableMiddlewarePipeInterface.php
+++ src/IterableMiddlewarePipeInterface.php
@@ -7,7 +7,14 @@ namespace Laminas\Stratigility;
 use IteratorAggregate;
 use Psr\Http\Server\MiddlewareInterface;
 
-/** @extends IteratorAggregate<int, MiddlewareInterface> */
+/**
+ * Implementors of this interface expose composed middleware as an iterable.
+ *
+ * This is useful for inspecting the middleware that is configured to run within a pipeline. Iterating over the pipeline
+ * should *not* cause side effects such as de-queueing any composed middleware.
+ *
+ * @extends IteratorAggregate<int, MiddlewareInterface>
+ */
 interface IterableMiddlewarePipeInterface extends MiddlewarePipeInterface, IteratorAggregate
 {
 }
